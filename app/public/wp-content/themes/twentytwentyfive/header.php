dad3068f5f7037ead6112f57b714c863430a3b4f
Samuel Hovie <samhovie@umich.edu>
2025-03-03 02:07:28 -0500

    site files

---
+++ app/public/wp-content/themes/twentytwentyfive/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+<div class="wp-site-blocks">
+    <header class="wp-block-template-part nav">
+        <?php echo do_blocks('<!-- wp:template-part {"slug":"header"} /-->'); ?>
+    </header>
+</div>
